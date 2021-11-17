b691a07d1d75704e9c8d64446028856cb9b3a616
RomeoAlex <romeo.alessandro17@gmail.com>
2021-11-17 17:40:20 +0100

    ulltimato

--- Gioco dei dadi/js/script.js
+++ Gioco dei dadi/js/script.js
@@ -1,10 +1,10 @@
-let pcNumber = Math.random(6);
+let pcNumber = parseInt(Math.floor(Math.random() * 5 + 1));
 alert("Il numero del banco è " + pcNumber);
-let userNumber = Math.random(6);
+let userNumber = parseInt(Math.floor(Math.random() * 5 + 1));
 alert("Il tuo numero è " + userNumber);
 if(pcNumber > userNumber){
     alert("Il banco ha vinto perchè " + pcNumber + " è maggiore del tuo numero " + userNumber);
-}else if(pcNumber > userNumber){
+}else if(userNumber > pcNumber){
     alert("Tu ha vinto perchè " + userNumber + " è maggiore del numero del banco " + pcNumber);
 }else{
     alert("Avete pareggiato perchè " + userNumber + " è uguale a " + pcNumber);
